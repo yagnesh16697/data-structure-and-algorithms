caaa587700cbe62109e552d0fdec9d9056d2f598
yagnesh16697 <yagnesh16697@gmail.com>
2022-10-24 13:05:28 +0530

    formatted code

--- Graphs/graph.cpp
+++ Graphs/graph.cpp
@@ -1,23 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-//graph-adjacency-list
-//space: O(N + 2 * E)
-int main(){
-    // numbers of nodes,edges
-    int n,m;
-    cin>>n>>m;
+// graph-adjacency-list
+// space: O(N + 2 * E)
+int main() {
+  // numbers of nodes,edges
+  int n, m;
+  cin >> n >> m;
 
-    vector<int>adj[n+1];
-    for(int i=0;i<m;i++){
-        int u,v;
-        cin>>u>>v;
+  vector<int> adj[n + 1];
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
 
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
 
-    return 0;
+  return 0;
 }
 
 // //graph-adjacency-list-weighted
@@ -40,7 +40,7 @@ int main(){
 // }
 
 // //graph-adjacency-matrix
-// //space:O(n * m)    
+// //space:O(n * m)
 // int main(){
 //     int n,m;
 //     cin>>n>>m;
@@ -50,8 +50,8 @@ int main(){
 //     for(int i=0;i<m;i++){
 //         int u,v;
 //         cin>>u>>v;
-//         adj[u][v] = 1;  
-//         adj[v][u] = 1; 
+//         adj[u][v] = 1;
+//         adj[v][u] = 1;
 //     }
 
 //     return 0;
